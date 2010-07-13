6498febf552fc5925743a99a0403c4e3df9d2f80
soltis <soltis@o2.pl>
2010-07-13 21:16:41 +0200

    Dodanie zmiennych.

--- let_wpa.sh
+++ let_wpa.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-SSID=$1
-PASS=$2
+SSID="$1"
+PASS="$2"
 
 mv /etc/wpa_supplicant.conf /etc/wpa_supplicant.conf.original
 
-wpa_passphrase myssid mypassphrase > /etc/wpa_supplicant.conf
+wpa_passphrase "${SSID}" "${PASS}" > /etc/wpa_supplicant.conf
 
 ifconfig wlan0 up
 
-iwconfig wlan0 essid ${SSID}
+iwconfig wlan0 essid "${SSID}"
 
 wpa_supplicant -B -Dwext -i wlan0 -c /etc/wpa_supplicant.conf
 dhcpcd wlan0
--- nagrywanie.sh
+++ nagrywanie.sh
@@ -3,7 +3,6 @@
 DIR=${HOME}
 NAME="${DIR}/nagranie.mpg"
 
-
 #ffmpeg -f x11grab -s 1280x800 -r 25 -i :0.0 /tmp/out.mpg
 
 ffmpeg -f x11grab -s 1280x800 -r 25 -i :0.0 "${NAME}" 
